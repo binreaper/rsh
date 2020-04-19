07f6461008ba49d2e3d8d9f5e44223e55057189e
Siddharth Gelera <ahoy@barelyhuman.dev>
2020-04-19 19:39:35 +0530

    base shell

---
+++ src/main.rs
@@ -0,0 +1,39 @@
+use std::env;
+use std::io::*;
+use std::path::Path;
+use std::process::Command;
+
+fn main() {
+    loop {
+        print!("> ");
+        stdout().flush().unwrap();
+
+        let mut input = String::new();
+        stdin().read_line(&mut input).unwrap();
+
+        let mut parts = input.trim().split_whitespace();
+        let command = parts.next().unwrap();
+        let args = parts;
+
+        match command {
+            "cd" => {
+                let new_dir = args.peekable().peek().map_or("/", |x| *x);
+                let root = Path::new(new_dir);
+                if let Err(e) = env::set_current_dir(&root) {
+                    eprintln!("{}", e);
+                }
+            }
+            "exit" => return,
+            command => {
+                let child = Command::new(command).args(args).spawn();
+
+                match child {
+                    Ok(mut child) => {
+                        child.wait().ok();
+                    }
+                    Err(e) => eprintln!("{}", e),
+                };
+            }
+        }
+    }
+}
